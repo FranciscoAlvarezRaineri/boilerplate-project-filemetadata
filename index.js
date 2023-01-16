170dc0aa602e7f682d35eb2c59c9213693d65be4
Replit user <>
2023-01-16 11:45:22 +0000

    solved

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser')
+const morgan = require("morgan");
+const multer = require("multer");
 require('dotenv').config()
 
 var app = express();
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan("tiny"));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+    console.log(req.file);
+  const {originalname, mimetype, size} = req.file
+    res.send({ name: originalname, type: mimetype, size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
